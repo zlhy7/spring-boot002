4646aa1dee729d2b00a0863bf168442e72918c33
zlhy7 <1833189645@qq.com>
2019-08-20 18:19:01 +0800

    随便写

--- src/main/java/com/renyong/springboot002/SpringBoot002Application.java
+++ src/main/java/com/renyong/springboot002/SpringBoot002Application.java
@@ -2,11 +2,11 @@ package com.renyong.springboot002;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
-//@ImportResource(locations = {"classpath:spring/spring-beans.xml"})
+@PropertySource("classpath:jdbc.properties")
 @SpringBootApplication
 public class SpringBoot002Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot002Application.class, args);
 	}
